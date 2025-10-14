201f0a7c3a6880effc49d962c2e415c17c5c198e
Kevin McDermott-Carpenter <kevin.mcdermott-carpenter@concentechltd.co.uk>
2025-10-14 23:29:23 +0100

    initial-commit

---
+++ lib/fluent_tag.dart
@@ -0,0 +1,5 @@
+export './src/tag.dart'
+    show Tag, DismissibleTag, InteractiveTag, DismissibleInteractiveTag;
+export './src/tag_style.dart' show TagStyle;
+export './src/tag_size.dart' show TagSize;
+export './src/tag_shape.dart' show TagShape;
